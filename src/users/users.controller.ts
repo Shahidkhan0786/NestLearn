7dbffafe937defc2a97634e4849b9100b822f1ee
Shahid Khan <130751667+shahid-khan-codegenio@users.noreply.github.com>
2023-06-01 06:45:57 +0500

    some changes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post,Param,Get} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private user:UsersService){}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    return body;
+    return this.user.create(body.email , body.password);
   }
+
+  @Get('/:id')
+  findUser(@Param() param){
+    return this.user.findOne(param.id);
+  }
+
+  findAllUsers(){
+
+  }
+
+  updateUser(){}
+
+  removeUser(){}
 }
