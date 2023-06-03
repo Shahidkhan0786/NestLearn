426e2a8874091ba09e7e6f9858324cbd8312dd8f
Shahid Khan <130751667+shahid-khan-codegenio@users.noreply.github.com>
2023-06-03 18:17:56 +0500

    xyz

---
+++ src/users/interceptors/serialize.interceptor.ts
@@ -0,0 +1,37 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    // run something before the request handler
+    // console.log('I am running before the request handler', context);
+
+    return next.handle().pipe(
+      map((data: any) => {
+        // run something before the response is sent out
+        // console.log('I am running before response is sent out', data);
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,50 @@
-import { Body, Controller, Post,Param,Get} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Param,
+  Get,
+  Put,
+  Delete,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { updateUserDto } from './dtos/update-user.dto';
+// import { SerializeInterceptor } from './interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+import { Serialize } from './interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
-  constructor(private user:UsersService){}
+  constructor(private user: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    return this.user.create(body.email , body.password);
+    return this.user.create(body.email, body.password);
   }
 
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id')
-  findUser(@Param() param){
-    return this.user.findOne(param.id);
+  async findUser(@Param('id') id: string) {
+    const user = await this.user.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
-  findAllUsers(){
+  findAllUsers() {}
 
+  @Put('/update/:id')
+  updateUser(@Body() body: updateUserDto, @Param('id') id: string) {
+    return this.user.update(parseInt(id), body);
   }
 
-  updateUser(){}
-
-  removeUser(){}
+  @Delete('/delete/:id')
+  removeUser(@Param('id') id: string) {
+    return this.user.remove(parseInt(id));
+  }
 }
