a3eef65e44c0a8a0e61620068150688390e4927b
Shahid Khan <130751667+shahid-khan-codegenio@users.noreply.github.com>
2023-06-05 18:21:36 +0500

    some changes

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,15 +1,36 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
+const scrypt = promisify(_scrypt);
 @Injectable({})
 export class AuthService {
   constructor(private user: UsersService) {}
 
-  async signup({ email: string, password: string }) {
+  async signup(email: string, password: string) {
     const users = await this.user.find(email);
     if (users.length) {
       throw new BadRequestException('email in use');
     }
+    const salt = randomBytes(8).toString('hex');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = await this.user.create(email, result);
+    return user;
   }
-  signin() {}
+  async signin(email: string, password: string) {
+    const [user] = await this.user.find(email);
+    const [salt, passwordhash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    if (passwordhash !== hash.toString('hex')) {
+      throw new BadRequestException('bad password');
+      // return user;
+    }
+
+    return user;
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   NotFoundException,
   UseInterceptors,
   ClassSerializerInterceptor,
+  Session,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -16,13 +17,25 @@ import { updateUserDto } from './dtos/update-user.dto';
 // import { SerializeInterceptor } from './interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { Serialize } from './interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class UsersController {
-  constructor(private user: UsersService) {}
+  constructor(private user: UsersService, private authService: AuthService) {}
+  @Serialize(UserDto)
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.user.create(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Serialize(UserDto)
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   // @UseInterceptors(new SerializeInterceptor(UserDto))
